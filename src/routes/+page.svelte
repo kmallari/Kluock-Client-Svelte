732cee8af3987a14b1c7371eb2eaf5806aeb23b0
Kevin Mallari <kevinmallari62@gmail.com>
2023-02-17 21:12:18 +0800

    init

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,30 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	const tabs = [
+		{ name: 'Vault', icon: 'vault' },
+		{ name: 'Autofill', icon: 'autofill' },
+		{ name: 'Generate', icon: 'generate' },
+		{ name: 'Settings', icon: 'settings' }
+	];
+
+	const handleTabClick = (tab: { name: string; icon: string }) => {
+		return () => {
+			console.log(tab);
+		};
+	};
+</script>
+
+<main class="w-[350px] h-[560px] bg-red-400 border border-black p-4">
+	<div class="flex flex-col h-full">
+		<div class="flex flex-col flex-grow">
+			<slot />
+		</div>
+		<div class="flex flex-row justify-between">
+			{#each tabs as tab}
+				<button class="flex flex-col items-center" on:click={handleTabClick(tab)}>
+					{tab.icon}
+					{tab.name}
+				</button>
+			{/each}
+		</div>
+	</div>
+</main>
